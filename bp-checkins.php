e50488d73d5e52ad44f7d6bc0e858c5b60f6e76d
Pallavi Gupta <pallavi@wbcomdesigns.com>
2017-07-25 12:54:08 +0530

    changed code to show admin notice for plugin dependency
    changed code to show admin notice for plugin dependency

--- bp-checkins.php
+++ bp-checkins.php
@@ -1,38 +1,28 @@
 <?php
-/*
-Plugin Name: BuddyPress Check-In
-Plugin URI: https://wbcomdesigns.com/contact/
-Description: This plugin adds extra feature to BuddyPress Activity updates allowing users to check-ins.
-Version: 1.0.5
-Author: Wbcom Designs
-Author URI: https://wbcomdesigns.com
-Text Domain: bp-checkins
-*/
+/**
+ * Plugin Name: BuddyPress Check-In
+ * Plugin URI: https://wbcomdesigns.com/contact/
+ * Description: This plugin adds extra feature to BuddyPress Activity updates allowing users to check-ins.
+ * Version: 1.0.5
+ * Author: Wbcom Designs
+ * Author URI: https://wbcomdesigns.com
+ * Text Domain: bp-checkins
+ */
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+global $bp_checkin;
+define( 'BPCHK_TEXT_DOMAIN', 'bp-checkins' );
 
-define( 'BPCHK_PLUGIN_PATH', plugin_dir_path(__FILE__) );
-define( 'BPCHK_PLUGIN_URL', plugin_dir_url(__FILE__) );
-
-//Include needed files
-$include_files = array(
-	'admin/bpchk-admin.php',
-	'includes/bpchk-scripts.php',
-	'includes/bpchk-filters.php',
-	'includes/bpchk-ajax.php',
-);
-foreach( $include_files  as $include_file ) {
-	include $include_file;
-}
-
-//BP Checkins Plugin Activation
-register_activation_hook( __FILE__, 'bpchk_plugin_activation' );
-function bpchk_plugin_activation() {
-	//Check if "Buddypress" plugin is active or not
-	if (!in_array('buddypress/bp-loader.php', apply_filters('active_plugins', get_option('active_plugins')))) {
-		//Buddypress Plugin is inactive, hence deactivate this plugin
-		deactivate_plugins( plugin_basename( __FILE__ ) );
-		wp_die( 'The <b>BP Checkins</b> plugin requires <b>Buddypress</b> plugin to be installed and active. Return to <a href="'.admin_url('plugins.php').'">Plugins</a>' );
-	}
+function run_bp_checkin_plugin() {
+	define( 'BPCHK_PLUGIN_PATH', plugin_dir_path(__FILE__) );
+	define( 'BPCHK_PLUGIN_URL', plugin_dir_url(__FILE__) );
+	
+	$include_files = array(
+		'admin/bpchk-admin.php',
+		'includes/bpchk-scripts.php',
+		'includes/bpchk-filters.php',
+		'includes/bpchk-ajax.php',
+	);
+	foreach( $include_files  as $include_file ) include $include_file;
 }
 
 /* custom post type function */
@@ -51,9 +41,30 @@ function create_place_post_type() {
 }
 add_action( 'init', 'create_place_post_type' );
 
-//Settings link for this plugin
-add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), 'bpchk_settings_link' );
-function bpchk_settings_link( $links ){
-	$settings_link = array( '<a href="'.admin_url('admin.php?page=bp-checkins').'">Setup</a>' );
+/**
+ * Check plugin requirement on plugins loaded
+ * this plugin requires buddypress to be installed and active
+ */
+add_action('plugins_loaded', 'bpchk_plugin_init');
+function bpchk_plugin_init() {
+	$bp_active = in_array('buddypress/bp-loader.php', get_option('active_plugins'));
+	if ( current_user_can('activate_plugins') && $bp_active !== true ) {
+		add_action('admin_notices', 'bpchk_plugin_admin_notice');
+	} else {
+		run_bp_checkin_plugin();
+		add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), 'bpchk_admin_settings_link' );
+	}
+}
+
+function bpchk_plugin_admin_notice() {
+	$bpfit_plugin = 'BuddyPress Check-In';
+	$bp_plugin = 'BuddyPress';
+
+	echo '<div class="error"><p>'.sprintf(__('%1$s is ineffective now as it requires %2$s to function correctly.', BPCHK_TEXT_DOMAIN ), '<strong>' . esc_html($bpfit_plugin) . '</strong>', '<strong>' . esc_html($bp_plugin) . '</strong>').'</p></div>';
+	if (isset($_GET['activate'])) unset($_GET['activate']);
+}
+
+function bpchk_admin_settings_link( $links ){
+	$settings_link = array( '<a href="'.admin_url('admin.php?page=bp-checkins').'">'.__( 'Settings', BPCHK_TEXT_DOMAIN ).'</a>' );
 	return array_merge( $links, $settings_link );
 }
